56d801826af432b245abd8b00efd367d20ec4d1c
Trourest186 <74035725+Trourest186@users.noreply.github.com>
2023-11-05 10:11:18 +0700

    Application for Devops-Project

--- Application/start.sh
+++ Application/start.sh
@@ -5,7 +5,7 @@ echo "-- init.sql" > $HOME/init.sql
 echo "CREATE DATABASE test;" >> $HOME/init.sql
 
 # Run the MySQL container with the init.sql file mounted
-docker run -d --name mysqlsrv1 -e MYSQL_ROOT_PASSWORD=a123456 -e MYSQL_ROOT_HOST=% -p 3306:3306 -v $HOME/init.sql:/docker-entrypoint-initdb.d/init.sql mysql
+docker run -d --name mysqlsrv1 -e MYSQL_ROOT_PASSWORD=a123456 -e MYSQL_ROOT_HOST=% -p 3306:3306 -v $HOME/init.sql:/docker-entrypoint-initdb.d/init.sql --restart always mysql
 
 # Run the Django container
 docker run -d -p 8000:8000 trourest186/django-project
