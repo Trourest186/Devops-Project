3dc8795cf05eb060d130f9dff7f3117f1e918e09
Trourest186 <74035725+Trourest186@users.noreply.github.com>
2023-11-28 09:35:59 +0700

    Terraform

--- Terraform/Executable_files/install_Jenkins.sh
+++ Terraform/Executable_files/install_Jenkins.sh
@@ -27,3 +27,11 @@ curl -sSL https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 
 
 # Install Nginx to setup Nginx layer 7
 sudo apt install nginx 
+
+# Install kubectl for aiming controller with base node role
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
+echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
+
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+kubectl version --client
